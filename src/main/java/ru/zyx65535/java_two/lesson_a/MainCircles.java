d9a8e524ed96a9567cdb38464065acfa743c980a
zyx65535 <zyx65535@yandex.ru>
2022-04-10 18:24:08 +0300

    Revert "lesson A"
    This reverts commit a5cf0bc629dc53d342165bf6a9060e8babfcb501.

--- src/main/java/ru/zyx65535/java_two/lesson_a/MainCircles.java
+++ src/main/java/ru/zyx65535/java_two/lesson_a/MainCircles.java
@@ -2,11 +2,6 @@ package ru.zyx65535.java_two.lesson_a;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class MainCircles extends JFrame {
     private static final int POS_X = 400;
@@ -16,31 +11,10 @@ public class MainCircles extends JFrame {
 
     private Ball[] sprites = new Ball[10];
 
-    private static Ball [] extendSpritesSize (Ball [] array) {
-        Ball [] temp = array.clone();
-        array = new Ball[array.length + 1];
-        System.arraycopy(temp,0,array,0, temp.length);
-        array[array.length - 1] = new Ball();
-        return array;
-    }
-
-    private static Ball [] decreaseSpritesSize (Ball [] array) {
-        Ball [] temp = array.clone();
-        array = new Ball[array.length - 1];
-
-        System.arraycopy(temp,0, array,0,  temp.length - 1);
-        return array;
-    }
-
-
-
-
     private void update(GameCanvas canvas, float deltaTime) {
         for (int i = 0; i < sprites.length; i++) {
             sprites[i].update(canvas, deltaTime);
         }
-
-
     }
 
     private void render(GameCanvas canvas, Graphics g) {
@@ -67,69 +41,10 @@ public class MainCircles extends JFrame {
         add(canvas);
         initApplication();
         setVisible(true);
-
-        canvas.addMouseListener(new MouseListener() {
-            public void mousePressed(MouseEvent me) {
-            }
-
-            public void mouseReleased(MouseEvent me) {
-            }
-
-            public void mouseEntered(MouseEvent me) {
-            }
-
-            public void mouseExited(MouseEvent me) {
-            }
-
-            public void mouseClicked(MouseEvent me) {
-                    if (me.getButton() == MouseEvent.BUTTON1) {
-                        sprites = extendSpritesSize(sprites);
-                        setTitle("Circles: " + String.valueOf((sprites.length)));
-                    }
-
-                    if (me.getButton() == MouseEvent.BUTTON3) {
-                        if (sprites.length != 0) {
-
-                        try {
-                            sprites = decreaseSpritesSize(sprites);
-                            setTitle("Circles: " + String.valueOf((sprites.length)));
-                        } catch (ArrayIndexOutOfBoundsException e1){
-                             {
-                                e1.printStackTrace();
-                            }
-
-                        }
-                    } else {
-                            setTitle("You already killed all of circles =(");
-                        }
-
-                }
-            }
-
-
-
-        });
-
-
-                try {
-            while (true) {
-                Thread.sleep(2000);
-                double r1 = (Math.random() * 32) - 1;
-                double g1 = (Math.random() * 32) - 1;
-                double b1 = (Math.random() * 32) - 1;
-                canvas.setBackground(new Color((int) r1, (int) g1, (int) b1));
-//                sprites = extendSpritesSize(sprites);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
-    private void addSprite () {
-        //        Collections.addAll(sprites[], new spritesList());
-    }
-
-    public static void main(String[] args) {new MainCircles();
+    public static void main(String[] args) {
+        new MainCircles();
     }
 
 }
